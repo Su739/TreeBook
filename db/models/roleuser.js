0bce344f8e6cb02d7ca1e066244f038d469f312b
Su739 <546367194@qq.com>
2018-03-04 14:33:14 +0800

    add references in roleuser model

--- db/models/roleuser.js
+++ db/models/roleuser.js
@@ -1,15 +1,29 @@
 'use strict';
+const User = require('./user.js');
+const Role = require('./role.js');
 module.exports = (sequelize, DataTypes) => {
   var RoleUser = sequelize.define('RoleUser', {
     roleId: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      primaryKey: true
+      primaryKey: true,
+      references: {
+        model: Role,
+        key: 'roleId'
+      },
+      onUpdate: 'cascade',
+      onDelete: 'cascade'
     },
     userId: {
       type: DataTypes.UUID,
       allowNull: false,
-      primaryKey: true
+      primaryKey: true,
+      references: {
+        model: User,
+        key: 'id'
+      },
+      onUpdate: 'cascade',
+      onDelete: 'cascade'
     }
   }, {});
   RoleUser.associate = function(models) {
