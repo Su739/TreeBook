accd3980954a72f2b9d35ed1851196a6a17b4b17
Su739 <546367194@qq.com>
2018-05-15 19:06:14 +0800

    .

--- db/seeders/20180420023835-demo-user-su-articles.js
+++ db/seeders/20180420023835-demo-user-su-articles.js
@@ -6,7 +6,7 @@ module.exports = {
       {
         title: 'demo',
         depth: 0,
-        parent: 10,
+        parent: 12,
         superior: 0,
         content: '\n# Live demo\n\nChanges are automatically rendered as you type.\n\n* Implements [GitHub Flavored Markdown](https://github.github.com/gfm/)\n* Renders actual, "native" React DOM elements\n* Allows you to escape or skip HTML (try toggling the checkboxes above)\n* If you escape or skip the HTML, no `dangerouslySetInnerHTML` is used! Yay!\n\n## HTML block below\n\n<blockquote>\n  This blockquote will change based on the HTML settings above.\n</blockquote>\n\n## How about some code?\n```js\nvar React = require("react");\nvar Markdown = require("react-markdown");\n\nReact.render(\n  <Markdown source="# Your markdown here" />,\n  document.getElementById("content")\n);\n```\n\nPretty neat, eh?\n\n## Tables?\n\n| Feature | Support |\n| ------ | ----------- |\n| tables | ✔ |\n| alignment | ✔ |\n| wewt | ✔ |\n\n## More info?\n\nRead usage information and more on [GitHub](//github.com/rexxars/react-markdown)\n\n---------------\n\nA component by [VaffelNinja](http://vaffel.ninja) / Espen Hovlandsdal\n',
         order: 1,
@@ -17,7 +17,7 @@ module.exports = {
       {
         title: 'demo2',
         depth: 0,
-        parent: 10,
+        parent: 12,
         superior: 0,
         content: '\n# Live demo\n\nChanges are automatically rendered as you type.\n\n* Implements [GitHub Flavored Markdown](https://github.github.com/gfm/)\n* Renders actual, "native" React DOM elements\n* Allows you to escape or skip HTML (try toggling the checkboxes above)\n* If you escape or skip the HTML, no `dangerouslySetInnerHTML` is used! Yay!\n\n## HTML block below\n\n<blockquote>\n  This blockquote will change based on the HTML settings above.\n</blockquote>\n\n## How about some code?\n```js\nvar React = require("react");\nvar Markdown = require("react-markdown");\n\nReact.render(\n  <Markdown source="# Your markdown here" />,\n  document.getElementById("content")\n);\n```\n\nPretty neat, eh?\n\n## Tables?\n\n| Feature | Support |\n| ------ | ----------- |\n| tables | ✔ |\n| alignment | ✔ |\n| wewt | ✔ |\n\n## More info?\n\nRead usage information and more on [GitHub](//github.com/rexxars/react-markdown)\n\n---------------\n\nA component by [VaffelNinja](http://vaffel.ninja) / Espen Hovlandsdal\n',
         order: 2,
@@ -28,8 +28,8 @@ module.exports = {
       {
         title: 'demo3',
         depth: 1,
-        parent: 10,
-        superior: 1,
+        parent: 12,
+        superior: 15,
         content: '\n# Live demo\n\nChanges are automatically rendered as you type.\n\n* Implements [GitHub Flavored Markdown](https://github.github.com/gfm/)\n* Renders actual, "native" React DOM elements\n* Allows you to escape or skip HTML (try toggling the checkboxes above)\n* If you escape or skip the HTML, no `dangerouslySetInnerHTML` is used! Yay!\n\n## HTML block below\n\n<blockquote>\n  This blockquote will change based on the HTML settings above.\n</blockquote>\n\n## How about some code?\n```js\nvar React = require("react");\nvar Markdown = require("react-markdown");\n\nReact.render(\n  <Markdown source="# Your markdown here" />,\n  document.getElementById("content")\n);\n```\n\nPretty neat, eh?\n\n## Tables?\n\n| Feature | Support |\n| ------ | ----------- |\n| tables | ✔ |\n| alignment | ✔ |\n| wewt | ✔ |\n\n## More info?\n\nRead usage information and more on [GitHub](//github.com/rexxars/react-markdown)\n\n---------------\n\nA component by [VaffelNinja](http://vaffel.ninja) / Espen Hovlandsdal\n',
         order: 1,
         public: false,
