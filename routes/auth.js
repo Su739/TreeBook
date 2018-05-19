6e1b35841252a64187137c57b10a9b3f6ed753b7
Su739 <546367194@qq.com>
2018-05-19 11:58:43 +0800

    .

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,7 @@ const router = express.Router();
 router.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'localhost:3000');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
