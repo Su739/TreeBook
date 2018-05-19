f8c59bce0c62a038d77757fb705f3a0ded14edd3
Su739 <546367194@qq.com>
2018-05-19 11:41:21 +0800

    .

--- middleware/ensureLoggedIn.js
+++ middleware/ensureLoggedIn.js
@@ -1,7 +1,7 @@
 module.exports = function ensureLoggedIn() {
   return function(req, res, next) {
     if (!req.isAuthenticated || !req.isAuthenticated()) {
-      return res.status(302).json({error: 'not authentication'});
+      return res.status(401).json({error: 'not authentication'});
     }
     next();
   };
--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const { validUserName, validRegister } = require('../authorization/lib/validator
 const router = express.Router();
 
 router.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'localhost:3000');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
