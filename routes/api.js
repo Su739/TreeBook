01cb3d76136a626fa58dcddb4b9ad663665b10e5
Su739 <546367194@qq.com>
2018-06-15 13:46:43 +0800

    add Access-Control-Expose-Headers

--- routes/api.js
+++ routes/api.js
@@ -20,6 +20,7 @@ router.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Expose-Headers', 'link');
   next();
 });
 
