d0e526ab90e026d28a0c321cd180d0c16b3eed19
Su739 <546367194@qq.com>
2018-07-04 09:54:58 +0800

    add allow-methods delete

--- routes/api.js
+++ routes/api.js
@@ -21,7 +21,7 @@ router.use(function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Expose-Headers', 'link');
-  res.header('Acess-COntrol-Allow-Methods', 'GET, POST, DELETE');
+  res.header('Access-COntrol-Allow-Methods', 'GET, POST, DELETE');
   next();
 });
 
