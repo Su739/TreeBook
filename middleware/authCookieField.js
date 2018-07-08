a43c811dd209878cdfc6e3ae181fba07129f8587
Su739 <546367194@qq.com>
2018-07-08 13:06:34 +0800

    add domain in cookie

--- middleware/authCookieField.js
+++ middleware/authCookieField.js
@@ -2,14 +2,14 @@ function authCookieField(req, res, next) {
   const { isLogged } = req.cookies;
   if (!req.isAuthenticated || !req.isAuthenticated()) {
     if (isLogged) {
-      res.cookie('isLogged', false, { expires: new Date(Date.now() + 10 * 60 * 60 * 24 * 1000) });
-      res.cookie('userName', 'null', { expires: new Date(Date.now() + 10 * 60 * 60 * 24 * 1000) });
+      res.cookie('isLogged', false, { expires: new Date(Date.now() + 10 * 60 * 60 * 24 * 1000), domain: '.lg739.com' });
+      res.cookie('userName', 'null', { expires: new Date(Date.now() + 10 * 60 * 60 * 24 * 1000), domain: '.lg739.com' });
     }
     next();
   } else {
     if (!isLogged) {
-      res.cookie('isLogged', true, { expires: new Date(Date.now() + 10 * 60 * 60 * 24 * 1000) });
-      res.cookie('userName', req.user.dataValues.userName, { expires: new Date(Date.now() + 10 * 60 * 60 * 24 * 1000) });
+      res.cookie('isLogged', true, { expires: new Date(Date.now() + 10 * 60 * 60 * 24 * 1000), domain: '.lg739.com' });
+      res.cookie('userName', req.user.dataValues.userName, { expires: new Date(Date.now() + 10 * 60 * 60 * 24 * 1000), domain: '.lg739.com' });
     }
     next();
   }
