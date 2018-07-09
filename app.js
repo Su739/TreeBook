eb327660f1ff356cea835448567cef5e79fdb8b7
Su739 <546367194@qq.com>
2018-07-09 21:45:09 +0800

    increase cookie expire

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 
 app.use(session({
   cookie: {
-    maxAge: 5000000
+    maxAge: 10 * 24 * 60 * 60 * 1000
   },
   store: new RedisStore({ client: client }),
   secret: 'keybord cat',
