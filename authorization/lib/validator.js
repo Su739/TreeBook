14d11076f1120042dd658e867ae7e40a70fb485a
Su739 <546367194@qq.com>
2018-07-09 16:23:57 +0800

    update build

--- authorization/lib/validator.js
+++ authorization/lib/validator.js
@@ -1,5 +1,10 @@
 const Promise = require('bluebird');
 
+const email = value =>
+  (value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
+    ? 'Invalid email address'
+    : undefined);
+
 /**
  * @param {string} name 用户名
  * @param {Object} options 验证条件
@@ -8,6 +13,9 @@ function validUserName(req, options = { minLength: 2, maxLength: 18, pattern: /^
   const { minLength = 2, maxLength = 18, pattern = /^[a-zA-Z0-9_]+$/g } = options;
   const username = req.body.username;
   return new Promise((resolve, reject) => {
+    if (email(username)) {
+      resolve();
+    }
     if (!username || username.length > maxLength || username.length < minLength || !pattern.test(username)) {
       reject({ field: 'username', message: '用户名应该由2-18个字母、数字或者下划线组成' });
     } else {
